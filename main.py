feebdcb0415f43ffb7932b1d98d6dbfd5d7796f9
Pierre Kern <pierre.kern@epitech.eu>
2024-09-19 15:54:57 +0200

    [FIX] win condition

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import random
 board = [' ' for _ in range(10)]
 
 def insert_letter(letter, pos):
-    board[pos] = letter 
+    board[pos] = letter
 
 def is_space_free(pos):
     return board[pos] == ' '
 
-def printf_board(board):
+def print_board(board):
     print('   |   |')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + ' ')
     print('   |   |')
@@ -16,7 +16,6 @@ def printf_board(board):
     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + ' ')
     print('   |   |')
     print('-----------')
-    print('   |   |')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + ' ')
     print('   |   |')
 
@@ -27,7 +26,8 @@ def is_winner(bo, le):
            (bo[1] == le and bo[4] == le and bo[7] == le) or \
            (bo[2] == le and bo[5] == le and bo[8] == le) or \
            (bo[3] == le and bo[6] == le and bo[9] == le) or \
-           (bo[3] == le and bo[5] == le and bo[7] == le)
+           (bo[3] == le and bo[5] == le and bo[7] == le) or \
+           (bo[1] == le and bo[5] == le and bo[9] == le)
 
 def player_move():
     run = True
@@ -47,7 +47,7 @@ def player_move():
             print('Please type a number')
 
 def is_board_full(board):
-    return board.count(' ') == 1  # The board is full if there's only one empty space left
+    return board.count(' ') <= 1
 
 def comp_move():
     possible_moves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
@@ -82,29 +82,29 @@ def select_random(li):
 
 def main():
     print("Welcome to AI Tic Tac Toe!")
-    printf_board(board)
+    print_board(board)
 
     while not is_board_full(board):
         if not is_winner(board, 'O'):
             player_move()
-            printf_board(board)
+            print_board(board)
         else:
-            print('O won')
+            print('O won!')
             break
         
         if not is_winner(board, 'X'):
             move = comp_move()
             if move == 0:
-                print('Tie Game!')
+                break
             else:
                 insert_letter('O', move)
-                print('Computer placed an O in position', move)
-                printf_board(board)
+                print(f'Computer placed an O in position {move}')
+                print_board(board)
         else:
-            print('X won')
+            print('X won!')
             break
     
-    if is_board_full(board):
+    if is_board_full(board) and not is_winner(board, 'X') and not is_winner(board, 'O'):
         print('Tie Game!')
 
 if __name__ == "__main__":
